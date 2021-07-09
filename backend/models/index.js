77e6f24d7ef57e158b5f46b136663844003e297f
Mr-Rakshit <rakshitsinghoff@gmail.com>
2021-07-09 15:12:11 +0530

    working 1

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,11 +1,6 @@
 var mongoose = require('mongoose');
 
-
-<<<<<<< HEAD
 const uri = "mongodb+srv://user:user@sandbox.3xwpf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-=======
-const uri = process.env.DB_URI;
->>>>>>> 2e277e41eff7a6a876bc228cabdeefe0c091cdc5
 console.log(uri);
 mongoose.set('debug',true);
 mongoose.connect(uri, {
