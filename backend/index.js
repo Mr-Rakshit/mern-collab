9c174056d2297f307485df76a93609a5d79e703d
Mr-Rakshit <rakshitsinghoff@gmail.com>
2021-07-09 14:35:03 +0530

    working

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,11 @@ require('dotenv').config();
 var express = require('express');
 var demoRoutes = require('./routes/demoRoutes');
 var bodyParser = require('body-parser');
+<<<<<<< HEAD
 var indexdb = require("./models/index")
+=======
+
+>>>>>>> 2e277e41eff7a6a876bc228cabdeefe0c091cdc5
 var app = express();
 
 const PORT = 4000;
--- mern-collab/src/Display.js
+++ mern-collab/src/Display.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 const Display = () => {
+<<<<<<< HEAD
   const [userdata, setUserdata] = useState([]);
 
   useEffect(() => {
@@ -27,6 +28,28 @@ const Display = () => {
           </div>
         );
       })}
+=======
+  var [userdata, setUserdata] = useState([]);
+  
+  useEffect(() => {
+    axios.get('/api/userdata')
+      .then((response) => {
+        setUserdata(response.data);
+      })
+      .catch((err) => console.log(err))
+  }, [])
+
+  console.log("data: " + userdata[0].first_name);
+  
+  return (
+    <div>
+      {
+        // userdata.foreach(function(data) {
+        //   console.log(data.id + " - " + data.first_name + " - " + data.last_name + " - " + data.email);
+        // })
+        // console.log("data: " + userdata[0].first_name)
+      }
+>>>>>>> 2e277e41eff7a6a876bc228cabdeefe0c091cdc5
     </div>
   );
 };
